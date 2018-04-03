ff41f67e1b7a4085cce690687d15f7704c4632d3
Florian Thiery <web@florian-thiery.de>
2018-04-03 09:17:29 +0200

    add AlligatorEvent class

---
+++ alligator/src/main/java/de/rgzm/alligator/classes/AlligatorEvent.java
@@ -0,0 +1,23 @@
+package de.rgzm.alligator.classes;
+
+import java.util.HashMap;
+
+public class AlligatorEvent {
+
+    public String id = "";
+    public String name = "";
+    public double x = -42.0;
+    public double y = -42.0;
+    public double z = -42.0;
+    public double a = -42.0;
+    public double b = -42.0;
+    public boolean fixed = false;
+    public boolean startFixed = false;
+    public boolean endFixed = false;
+    public HashMap distances;
+    
+    public String toString() {
+        return id + " " + name + " " + x + " " + y + " " + z + " " + a + " " + b + " " + fixed + " " + startFixed + " " + endFixed;
+    }
+
+}
